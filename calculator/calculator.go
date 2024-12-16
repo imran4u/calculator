99c0461086ac0cc0484dfe5b9b08431204476f57
imran4u <er.imran4u@gmail.com>
2024-12-16 21:38:52 +0530

    make test and added some more test

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,11 +1,32 @@
 package calculator
 
+import "fmt"
+
 type Calculator struct{}
 
 func (c *Calculator) Add(a, b int) int {
 	return a + b
 }
 
+func (c *Calculator) sum(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
 func (c *Calculator) Subtract(a, b int) int {
 	return a - b
+}
+
+func (c *Calculator) Multiply(a, b int) int {
+	return a * b
+}
+
+func (c *Calculator) Div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("denominator should not be zero")
+	}
+	return a / b, nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -9,7 +9,19 @@ import (
 func TestAdd(t *testing.T) {
 	calc := Calculator{}
 	result := calc.Add(2, 3)
-	require.Equal(t, result, 5)
+	require.Equal(t, 5, result)
+	require.NotEmpty(t, result)
+
+}
+
+func TestSum(t *testing.T) {
+	calc := Calculator{}
+	result := calc.sum(2, 3)
+	require.Equal(t, 5, result)
+	require.NotEmpty(t, result)
+
+	result = calc.sum(2, 3, 4, 5, 6, 9)
+	require.Equal(t, 29, result)
 	require.NotEmpty(t, result)
 
 }
@@ -19,5 +31,33 @@ func TestSubtract(t *testing.T) {
 	result := calc.Subtract(2, 3)
 	require.Equal(t, -1, result)
 	require.NotEmpty(t, result)
+
+}
+
+func TestMultiply(t *testing.T) {
+	calc := Calculator{}
+	result := calc.Multiply(2, 3)
+	require.Equal(t, 6, result)
+	result = calc.Multiply(0, 10)
+	require.Empty(t, result)
+	require.Equal(t, 0, result)
+
+}
+
+func TestDivistion(t *testing.T) {
+	calc := Calculator{}
+	result, err := calc.Div(6, 3)
+	require.NoError(t, err)
+	require.Equal(t, 2, result)
+
+	result, err = calc.Div(0, 10)
+	require.NoError(t, err)
+	require.Empty(t, result)
+	require.Equal(t, 0, result)
+
+	result, err = calc.Div(100, 0)
+	require.Error(t, err)
+	require.Equal(t, "denominator should not be zero", err.Error())
+	require.Empty(t, result)
 
 }
