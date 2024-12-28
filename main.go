320cd15a7ad7370589d7e3e0e88ca3d90a48be96
hashmi846003 <109304853+hashmi846003@users.noreply.github.com>
2024-12-28 16:41:27 +0530

    first commit

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	"calculator/handler/calculator.go"
+	//"calculator/handler"
 	"github.com/imran4u/calculator/calculator"
 )
 
 func main() {
 	c := calculator.Calculator{}
 	fmt.Println(c.Add(2, 5))
-
+	r.POST("/add", handler.AddHandler)
 }
